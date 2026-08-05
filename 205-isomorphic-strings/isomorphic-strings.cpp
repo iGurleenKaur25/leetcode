a99091251efc877c4eda1b9c4351b4117cf8e216
Gurleen Kaur <igurleenkaur25@gmail.com>
2026-08-05 13:58:58 +0530

    Time: 7 ms (6.24%) | Memory: 9.5 MB (30.55%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.size() != t.size()) return false;
+
+        unordered_map<char,char > mp1;
+        unordered_map<char,char > mp2;
+    for(int i =0 ; i < s.size(); i++){
+        while(mp1.count(s[i]) && mp1[s[i]] != t[i]){
+            return false;
+        }
+        while(mp2.count(t[i]) && s[i] != mp2[t[i]]){
+            return false;
+        }
+
+        mp1[s[i]] = t[i];
+        mp2[t[i]] = s[i];
+    }
+    return true;
+        
+    }
+};
